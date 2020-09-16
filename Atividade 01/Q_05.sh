fecb474933a574c23321a48881914605f0a56a40
Larissa Galdino <redescomplarilg@gmail.com>
2020-09-16 23:44:04 +0000

    Atualização 04

--- Atividade 01/Q_05.sh
+++ Atividade 01/Q_05.sh
@@ -2,3 +2,10 @@
 
 echo "Questão 05: Escreva um script que: crie um diretório /bin/DATA onde DATA deve ser a data atual do sistema obtida a partir do comando date (no formato ano-mes-dia); e copie todos os arquivos do diretório atual para este novo diretório."
 
+DATA = "$(data +% Y.% M.% d)"
+PASTA = "/bin/$DATA"
+mkdir -p ${PASTA} 2>/dev/null
+
+ls -1 ./ >> /bin/DATA
+
+cp *  $PASTA
--- Atividade 01/Q_08.sh
+++ Atividade 01/Q_08.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
 echo "Questão 08: Escreva um script que imprime os conceitos de substituição de variáveis e de substituição de shell, com exemplos."
+
+echo -e 'A variável $0 mostra o comando executado, seu valor é '${0}''
+echo -e 'A variável $1 equivale ao primeiro argumento fornecido, seu valor é '${1}''
+echo -e 'A variável $$ equivale ao número de execução do processo, seu valor é '$$''
+echo -e 'A variável $* equivale a relação de argumentos passados, seu valor é '$*''
+echo -e 'A variável $@ equivale ao conjunto de argumentos, seu valor é '$@''
+echo -e 'A variável $? equivale ao código de retorno do último comando, seu valor é '$?''
+echo -e 'A variável $PPID equivale ao identificador de processos, seu valor é '${PPID}''
+echo -e 'A variável $PATH mostra o diretório válido para comandos, seu valor é '${PATH}''
+echo -e 'A variável $HOME mostra a raíz do presente usuário, seu valor é '${HOME}''
+echo -e 'A variável $PWD mostra o diretório atual, seu valor é '${PWD}''
