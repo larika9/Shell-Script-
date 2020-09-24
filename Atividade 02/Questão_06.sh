443898fe1115b744b2a1a2f9d0ec98b4dcc35f5c
Larissa Galdino <redescomplarilg@gmail.com>
2020-09-24 00:45:59 +0000

    .

--- Atividade 02/Questão_06.sh
+++ Atividade 02/Questão_06.sh
@@ -2,3 +2,11 @@
 
 echo "6º - Escreva um script que use o arquivo /etc/passwd para contar o número de usuários do sistema. Adicionalmente, use grep, wc -l e substituição de shell para verificar quantos usuários usam /bin/bash como interpretador de comandos padrão. Use aritmética do shell para calcular quantos usuários não usam /bin/bash como interpretador de comandos padrão."
 
+declare -i usuarios_bash=0
+
+numero_usuarios=$(cut -d: -f1 /etc/passwd | wc -l)
+usuarios_bin_bash=$(grep -v "/bim/bash" /etc/passwd | wc -l)))
+
+echo "Existe $numero_usuarios no sistemas:"
+echo "Existe $usuarios_bin_bash na Bash do sistema"
+echo "Existe $usuarios_bash que não utilizam o BASH."
