2bd9e02e60ba127866a2fb93f6752aee88853474
Larissa Galdino <redescomplarilg@gmail.com>
2020-12-15 18:56:25 +0000

    Criando Atividade 14

---
+++ Atividade_14/Questão_01.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo "1º - Escreva um script que exiba a data atual e uma mensagem motivacional aleatória. Faça que esse script seja executado todas as vezes que seu usuário faça login no sistema."
---
+++ Atividade_14/Questão_02.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo"2º - Escreva um script que remova todos os arquivos da pasta /home/<seu_user>/tmp. Faça que este script seja executado todas as vezes que o computador for ligado."
